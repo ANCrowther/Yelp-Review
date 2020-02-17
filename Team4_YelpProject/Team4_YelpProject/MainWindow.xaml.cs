d5d46f39394e39d2b829fa485de09f65cec7efb1
BobNineToes <acrowther014@gmail.com>
2020-02-16 18:53:34 -0800

    Built the addState and addGridColumns methods.

--- Team4_YelpProject/Team4_YelpProject/MainWindow.xaml.cs
+++ Team4_YelpProject/Team4_YelpProject/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
 
 namespace Team4_YelpProject
 {
@@ -40,6 +41,60 @@ namespace Team4_YelpProject
             return "Host = localhost; Username = postgre; Database = milestone1db; password = spiffy";
         }
 
+        private void addState()
+        {
+            using (var connection = new NpgsqlConnection(buildConnectionString()))
+            {
+                connection.Open();
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = connection;
+                    cmd.CommandText = "SELECT distinct state FROM business ORDER BY state";
+                    try
+                    {
+                        var reader = cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            StateList.Items.Add(reader.GetString(0));
+                        }
+                    }
+                    catch (NpgsqlException ex)
+                    {
+                        System.Windows.MessageBox.Show("SQL Error - " + ex.Message.ToString());
+                    }
+                    finally
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
 
+        private void addGridColumns()
+        {
+            DataGridTextColumn col1 = new DataGridTextColumn();
+            col1.Binding = new Binding("name");
+            col1.Header = "Business Name";
+            col1.Width = 255;
+            businessGrid.Columns.Add(col1);
+
+            DataGridTextColumn col2 = new DataGridTextColumn();
+            col2.Binding = new Binding("state");
+            col2.Header = "State";
+            col2.Width = 60;
+            businessGrid.Columns.Add(col2);
+
+            DataGridTextColumn col3 = new DataGridTextColumn();
+            col3.Binding = new Binding("city");
+            col3.Header = "City";
+            col3.Width = 150;
+            businessGrid.Columns.Add(col3);
+
+            DataGridTextColumn col4 = new DataGridTextColumn();
+            col4.Binding = new Binding("business_id");
+            col4.Header = "";
+            col4.Width = 0;
+            businessGrid.Columns.Add(col4);
+        }
     }
 }
