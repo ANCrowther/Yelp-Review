9bc64c0687ea0a9af2021e1f75b9855e35a34be6
BobNineToes <acrowther014@gmail.com>
2020-04-03 21:56:02 -0700

    Got the watermark to work. Made a user class. Now need to remake the userNameTextBox_KeyDown method to generate an user entity.

--- Team4_YelpProject/Team4_YelpProject/WatermarkConverter.cs
+++ Team4_YelpProject/Team4_YelpProject/WatermarkConverter.cs
@@ -1,22 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
 
 namespace Team4_YelpProject
 {
-    public class BoolVisibilityConverter : IMultiValueConverter
+    public class WatermarkConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            bool hasText = (bool)values[0];
+            bool hasText = !(bool)values[0];
             bool hasFocus = (bool)values[1];
 
-            if(!hasText || hasFocus)
+            if (hasText || hasFocus)
             {
-                return Visibility.Hidden; //.Collapsed
+                return Visibility.Collapsed;
             }
-
             return Visibility.Visible;
         }
 
